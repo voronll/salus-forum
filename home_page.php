6dc18c38acf9eee9a3b5c38d2f073fed0e8a3344
voronll <dudubachosky10@hotmail.com>
2024-08-30 21:53:18 -0300

    feat: add fontes e style

--- home_page.php
+++ home_page.php
@@ -1,16 +1,26 @@
+<?php include 'inc/header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Salus - Forum</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #000000;
+            color: #ffffff; /* Caso queira que o texto também fique branco para contrastar */
+        }
+    </style>
 </head>
+
 <body>
 
-    <h1>FIRST COMMIT!</h1>
     <!-- Bootstrap JS e dependências -->
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
